fa996118b24d7ff610edbd1f56cf5fbc575bb69c
GitHub Action <action@github.com>
2023-11-10 15:13:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/abhinaypandey/localegenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/abhinaypandey/localegenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.abhinaypandey.localegenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.abhinaypandey.localegenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
